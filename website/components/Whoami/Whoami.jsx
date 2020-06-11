f5c2463c125eb68e1e835ce41c40c41fdef544ac
imanol <imanol.rodriguez@fhios.es>
2020-06-11 19:52:28 +0200

    new look for sections

--- website/components/Whoami/Whoami.jsx
+++ website/components/Whoami/Whoami.jsx
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 export default (props) => (
-  <Container align="justify" style={{'margin-top': '-30px'}}>
-    <IconList align="center">
+  <Container align="center" style={{'margin-top': '-30px'}}>
+    <IconList>
       <a href="https://github.com/imrodriguez" rel="noreferrer" target="_blank" aria-label="Github"><FontAwesomeIcon icon={faGithub} size="xs" /></a>
       <a href="https://www.linkedin.com/in/imanol-rodriguez-de-maya-47bb1376/" rel="noreferrer" target="_blank" aria-label="Linkedin"><FontAwesomeIcon icon={faLinkedin} size="xs" /></a>
       <a href="https://www.instagram.com/metacharset/" rel="noreferrer" target="_blank" aria-label="Instagram"><FontAwesomeIcon icon={faInstagram} size="xs" /></a>
     </IconList>
-    <div dangerouslySetInnerHTML={{ __html: props.whoami[0].body }}></div>
+    <div align="justify" dangerouslySetInnerHTML={{ __html: props.whoami[0].body }}></div>
   </Container>
 )
