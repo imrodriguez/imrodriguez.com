7c9af8c71a1f4f8cc90b35edbbd00c133582bb8a
Imanol Rodríguez <imanolrodriguezdemaya@gmail.com>
2022-11-28 09:49:27 +0100

    Image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from "@astrojs/partytown";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,8 @@ export default defineConfig({
         config: {
           forward: ["dataLayer.push"],
         },
-      }),],
+      }),
+      image({
+        serviceEntryPoint: '@astrojs/image/sharp'
+      })],
 });
