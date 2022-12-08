64da97ec418e993cdc19bc56da723263a0fec79e
Imanol Rodríguez <imanolrodriguezdemaya@gmail.com>
2022-12-08 16:02:34 +0100

    robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,34 @@ import react from '@astrojs/react';
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+const noIndexPages = [
+  "https://imrodriguez.com/404/",
+  "https://imrodriguez.com/es/404/",
+  "https://imrodriguez.com/thankyou/",
+  "https://imrodriguez.com/es/thankyou/"
+]
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [astroI18next(),react(),tailwind(), mdx(), image({
-        serviceEntryPoint: '@astrojs/image/sharp'
-      })],
-      
+  site: "https://imrodriguez.com",
+  integrations: [astroI18next(), react(), tailwind(), mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap({
+    filter: (page) => {
+      if (!noIndexPages.includes(page)) {
+        return true;
+      } else {
+        return false;
+      }
+    }  ,
+    i18n: {
+      defaultLocale: "en",
+      locales: {
+        en: "en",
+        es: "es",
+      }
+    }
+  })]
 });
